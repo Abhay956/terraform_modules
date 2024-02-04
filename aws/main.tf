f358e75f9ebdb0654eecbc31422d81673816c365
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-02-04 17:47:14 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myinstance1" {
 ami = "ami-0277155c3f0ab2930"
 instance_type = "t2.micro"
-key_name = "TF-key"
+key_name = "ENTER_YOU_KEY_PAIR"
 associate_public_ip_address = true
 tags = {
         Name = "Webserver1"
